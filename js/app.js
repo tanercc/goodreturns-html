99cff51b85a2af82a0bbcd530c7d3459dac45365
Emir <86002529+EmirKerimEskici@users.noreply.github.com>
2022-06-27 02:23:49 +0300

    Emir Tarafindan eklendi

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function openFunction() {
+    document.getElementById("closeIcon").style.display = "block";
+    document.getElementById("searchInput").style.display="block";
+    document.getElementById("searchIcon").style.display="none";
+    }
+    function closeFunction() {
+        document.getElementById("searchIcon").style.display = "block";
+        document.getElementById("searchInput").style.display="none";
+        document.getElementById("closeIcon").style.display="none";
+        }
+
